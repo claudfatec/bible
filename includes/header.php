fcd3a7a1ad93092d7b95b1c8c9f163dfc2b8affe
claudfatec <92130393+claudfatec@users.noreply.github.com>
2022-08-09 08:41:45 -0300

    Changed icon

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="https://calm-coast-0dcfade10.1.azurestaticapps.net//book.png" />
         <link rel="stylesheet" href="includes/onestyle.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/react-grid-layout/1.3.4/css/styles.css" integrity="sha512-moa9vX1M9pP4U9aRPZ9/z5o2+I3OLJsWEvJ09MJNX9MunGXjeOtRrW04HCxacZvsxHLJOdNIpZkhbSZ+T1mW6w==" crossorigin="anonymous" referrerpolicy="no-referrer" />        <title>My Personal Bible</title>
     </head>
